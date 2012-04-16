68e99451d003906e2df7b2faed0cccc68c49cded
ksjfghfj <alvesky@yandex.ru>
2012-04-16 22:32:47 +0300

    change project structure

--- public/ace/index.php
+++ public/ace/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 define('ACE_DIR', '../../ace/');
-define('XSLT_DIR', ACE_DIR.'xslt/');
+define('XSLT_DIR', ACE_DIR.'ide/xslt/');
 
 require_once ACE_DIR.'lib/AceXMLElement.php';
 require_once ACE_DIR.'lib/functions.php';
@@ -13,6 +13,6 @@ $model = 'areas.xml';
 $data = new AceXMLElement('<data/>');
 $modelConfig = new AceXMLElement(ACE_DIR.'ide/'.$model, 0, true);
 $data->insertXmlElement($modelConfig->grid);
-$data->insertXmlFile(ACE_DIR.$model, 'items');
+$data->insertXmlFile(ACE_DIR."engine/$model", 'items');
 
-echo templateXSL(XSLT_DIR.'areas.xsl', $data);
+echo templateXSL('areas.xsl', $data);
