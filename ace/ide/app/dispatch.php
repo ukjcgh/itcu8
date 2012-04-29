2ce7c7475ee6c289dcd30821c18a7e5c4ffb47b9
ksjfghfj <alvesky@yandex.ru>
2012-04-29 17:50:37 +0300

    ref

--- ace/ide/app/dispatch.php
+++ ace/ide/app/dispatch.php
@@ -1,15 +1,15 @@
 <?php
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'default';
- 
+
 if(preg_match('~[^a-z]~', $action)){
 	trigger_error('Invalid action name', E_USER_ERROR);
 }
 
-$action_filename = ACE_DIR.'ide'.DS.'app'.DS.'actions'.DS.$action.'.php';
+$action_filename = IDE_DIR . 'app' . DS . 'actions' . DS . $action . '.php';
 
 if(is_readable($action_filename)) {
 	include $action_filename;
 } else {
-	trigger_error('Action "'.$action_filename.'" Not Found', E_USER_ERROR);
+	trigger_error('Action "' . $action_filename . '" Not Found', E_USER_ERROR);
 }
