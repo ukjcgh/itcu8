6b9b44fae1fe4a0d8fd1370587c0900230c30ea5
ksjfghfj <alvesky@yandex.ru>
2012-04-29 17:41:59 +0300

    refactoring, formatting

--- pub/ace/index.php
+++ pub/ace/index.php
@@ -5,10 +5,11 @@ error_reporting(E_ALL);
 //header('Content-type: text/plain');
 
 define('DS', DIRECTORY_SEPARATOR);
-define('ACE_DIR', realpath('..'.DS.'..'.DS.'ace').DS);
-define('TPL_DIR', ACE_DIR.'ide'.DS.'templates'.DS);
+define('ACE_DIR', realpath('..' . DS . '..' . DS . 'ace') . DS);
+define('IDE_DIR', ACE_DIR . 'ide' . DS);
+define('TPL_DIR', IDE_DIR . 'templates' . DS);
 
-require_once ACE_DIR.'ide'.DS.'lib'.DS.'functions.php';
-require_once ACE_DIR.'ide'.DS.'lib'.DS.'AceXMLElement.php';
+require_once IDE_DIR . 'lib' . DS . 'functions.php';
+require_once IDE_DIR . 'lib' . DS . 'AceXMLElement.php';
 
-require_once ACE_DIR.'ide'.DS.'app'.DS.'dispatch.php';
+require_once IDE_DIR . 'app' . DS . 'dispatch.php';
