19d4284a2ebf8bcf1105dedb3d7d9e5c7da585e8
ksjfghfj <alvesky@yandex.ru>
2012-04-30 00:18:24 +0300

    remove non existing js from html

--- ace/ide/app/actions/default.php
+++ ace/ide/app/actions/default.php
@@ -11,7 +11,6 @@ $page->head->title = "TTITLEE";
 $page->head->styles[] = 'styles.css';
 $page->head->styles[] = 'popup.css';
 $page->head->scripts[] = 'jquery-1.7.2.min.js.invalid';
-$page->head->scripts[] = 'ace.js';
 $page->head->scripts[] = 'popup.js';
 $page->head->scripts[] = 'grid.js';
 
