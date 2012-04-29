37345213b4e4eceb56e503ab708a7ebc4082e74a
ksjfghfj <alvesky@yandex.ru>
2012-04-29 16:11:21 +0300

    move lib to ide/lib

--- pub/ace/index.php
+++ pub/ace/index.php
@@ -8,7 +8,7 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ACE_DIR', realpath('..'.DS.'..'.DS.'ace').DS);
 define('XSLT_DIR', ACE_DIR.'ide'.DS.'xslt'.DS);
 
-require_once ACE_DIR.'lib'.DS.'functions.php';
-require_once ACE_DIR.'lib'.DS.'AceXMLElement.php';
+require_once ACE_DIR.'ide'.DS.'lib'.DS.'functions.php';
+require_once ACE_DIR.'ide'.DS.'lib'.DS.'AceXMLElement.php';
 
 require_once ACE_DIR.'ide'.DS.'app'.DS.'router.php';
