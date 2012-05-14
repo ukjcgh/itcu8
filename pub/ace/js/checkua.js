898c91763f9c7030fdf1fc0c2039ed1d6f485ec4
ksjfghfj <alvesky@yandex.ru>
2012-05-14 20:59:28 +0300

    browser check

--- pub/ace/js/checkua.js
+++ pub/ace/js/checkua.js
@@ -9,20 +9,24 @@ if (window.chrome) {
 }
 
 if (!browser) {
+
 	w = function(html) {
 		document.write(html);
 	}
 
 	knownInvalid = window.opera ? 'Opera' : (navigator.userAgent.indexOf('MSIE') >= 0 ? 'Internet Explorer' : false);
 
-	w('<div style="position: fixed; left: 0; top: 0; width: 100%; height: 100%; background-color: #fff; padding: 5px;">');
+	w('<div id="invalid-browser" style="position: fixed; left: 0; top: 0; width: 100%; height: 100%; background-color: #fff; padding: 10px;">');
 
-	proposition = 'Use Chrome or Firefox instead.';
+	proposition = 'Use <a href="https://www.google.com/chrome"><b>Chrome</b></a> or <a href="http://firefox.com"><b>Firefox</b></a> instead.';
 	if (knownInvalid) {
-		w(knownInvalid + '? It is thankless job to make me working with this browser so I don\'t support it. ' + proposition);
+		w(knownInvalid + '? In order to keep code cleaner we don\'t support this browser. ' + proposition);
 	} else {
 		w('Don\'t know this browser and how to work with it. ' + proposition);
 	}
 
+	w(' Functionality is not adapted for your browser and have never been tested.')
+
 	w('</div>');
+
 }
